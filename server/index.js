360fe57aef4beb783d7a4d16fc4ff1f621e0948b
Matthew Zipkes <matthewzipkes@gmail.com>
2021-10-27 13:02:26 -0500

    Added dependencies

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require(express);
-const cors = require(cors);
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json())
-app.use(express.static())
+app.use(express.static(__dirname + '/../client/dist'))
 
 app.listen(port, (error) => {
   if (error) {
