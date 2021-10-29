1850e0e9e3f2090965622eb46bbccae3494b7c88
Matthew Zipkes <matthewzipkes@gmail.com>
2021-10-29 13:14:17 -0500

    workig route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const cors = require('cors');
 const axios = require('axios');
 const API_KEY = require('./config.js');
 const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx';
+const config = {
+  headers : {
+    'Authorization':
+    API_KEY
+  }
+}
 
 const app = express();
 const port = 3000;
@@ -12,11 +18,12 @@ app.use(express.json())
 app.use(express.static(__dirname + '/../client/dist'))
 
 app.get('/products', (req, res) => {
-  axios.get(`${url}/products`, {'Authorization': API_KEY})
+  axios.get(`${url}/products`, config)
   .then(products => {
     res.status(200).send(products.data)
   })
   .catch(error => {
+    console.error(error);
     res.status(400).send()
   })
 })
