2b4168d8a599fbf9e71706da77a4ffc2f35c26bb
Alex Ni <cl.alexni@gmail.com>
2021-10-30 01:09:32 -0500

    remove cake

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
-import CakeContainer from './CakeContainer.js';
+
 import { Provider } from 'react-redux';
 import store from '../redux/store.js';
 
